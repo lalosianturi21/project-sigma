07d9b248f077f2fc38a8a8a0873a1f259e51437b
git-peto <rrudyhome@gmail.com>
2022-09-13 11:39:13 +0700

    contoh perubahan

--- app/Views/pages/dashboard.php
+++ app/Views/pages/dashboard.php
@@ -1,4 +1,4 @@
 <div class="container-fluid"  style="text-align: center">
-    <h3>Ini adalah halaman dashboard</h3>
+    <h3>Ini adalah halaman dashboard COBA COBA</h3>
     <img src="/images/bgapotek.jpg">
 </div>
